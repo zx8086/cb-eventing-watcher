5728eef4e76f0aeb7adf3ebb7cf07c43519a6a29
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-08-16 01:08:03 +0200

    minor fixes

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -20,7 +20,6 @@ import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import {
   MeterProvider,
   PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
 } from "@opentelemetry/sdk-metrics";
 import {
   LoggerProvider,
@@ -28,101 +27,127 @@ import {
 } from "@opentelemetry/sdk-logs";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { WinstonInstrumentation } from "@opentelemetry/instrumentation-winston";
+import {
+  envDetector,
+  hostDetector,
+  osDetector,
+  processDetector,
+} from "@opentelemetry/resources";
 
 import * as api from "@opentelemetry/api-logs";
 import { config } from "$config";
 
 // Set up diagnostics logging
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-// Create a shared resource
-const resource = new Resource({
-  [SEMRESATTRS_SERVICE_NAME]: config.openTelemetry.SERVICE_NAME,
-  [SEMRESATTRS_SERVICE_VERSION]: config.openTelemetry.SERVICE_VERSION,
-  [SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]:
-    config.openTelemetry.DEPLOYMENT_ENVIRONMENT,
-});
+async function setupTelemetry() {
+  // Create a shared resource
+  const resource = new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: config.openTelemetry.SERVICE_NAME,
+    [SEMRESATTRS_SERVICE_VERSION]: config.openTelemetry.SERVICE_VERSION,
+    [SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]:
+      config.openTelemetry.DEPLOYMENT_ENVIRONMENT,
+  });
 
-// Create OTLP exporters
-const traceExporter = new OTLPTraceExporter({
-  url: config.openTelemetry.TRACES_ENDPOINT,
-  headers: { "Content-Type": "application/json" },
-});
+  // Detect and merge additional resources
+  const detectors = [envDetector, hostDetector, osDetector, processDetector];
 
-const otlpMetricExporter = new OTLPMetricExporter({
-  url: config.openTelemetry.METRICS_ENDPOINT,
-  headers: { "Content-Type": "application/json" },
-});
+  for (const detector of detectors) {
+    const detectedResource = await detector.detect();
+    resource.merge(detectedResource);
+  }
 
-const logExporter = new OTLPLogExporter({
-  url: config.openTelemetry.LOGS_ENDPOINT,
-  headers: { "Content-Type": "application/json" },
-});
+  // Create OTLP exporters
+  const traceExporter = new OTLPTraceExporter({
+    url: config.openTelemetry.TRACES_ENDPOINT,
+    headers: { "Content-Type": "application/json" },
+  });
 
-// Set up LoggerProvider
-const loggerProvider = new LoggerProvider({ resource });
-loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
-api.logs.setGlobalLoggerProvider(loggerProvider);
+  const otlpMetricExporter = new OTLPMetricExporter({
+    url: config.openTelemetry.METRICS_ENDPOINT,
+    headers: { "Content-Type": "application/json" },
+  });
 
-// Set up MetricReaders
-const consoleMetricReader = new PeriodicExportingMetricReader({
-  exporter: new ConsoleMetricExporter(),
-  exportIntervalMillis: config.openTelemetry.CONSOLE_METRIC_READER_INTERVAL,
-});
+  const logExporter = new OTLPLogExporter({
+    url: config.openTelemetry.LOGS_ENDPOINT,
+    headers: { "Content-Type": "application/json" },
+  });
 
-const otlpMetricReader = new PeriodicExportingMetricReader({
-  exporter: otlpMetricExporter,
-  exportIntervalMillis: config.openTelemetry.METRIC_READER_INTERVAL,
-});
+  // Set up LoggerProvider
+  const loggerProvider = new LoggerProvider({ resource });
+  loggerProvider.addLogRecordProcessor(
+    new BatchLogRecordProcessor(logExporter),
+  );
+  api.logs.setGlobalLoggerProvider(loggerProvider);
 
-// Set up MeterProvider
-const meterProvider = new MeterProvider({
-  resource: resource,
-  // readers: [consoleMetricReader, otlpMetricReader],
-  readers: [otlpMetricReader],
-});
+  // Set up MetricReaders
+  const otlpMetricReader = new PeriodicExportingMetricReader({
+    exporter: otlpMetricExporter,
+    exportIntervalMillis: config.openTelemetry.METRIC_READER_INTERVAL,
+  });
 
-// Set this MeterProvider to be global to the app being instrumented. -> For multiple Metrics Readers
-metrics.setGlobalMeterProvider(meterProvider);
+  // Set up MeterProvider
+  const meterProvider = new MeterProvider({
+    resource: resource,
+    readers: [otlpMetricReader],
+  });
 
-// Node SDK for OpenTelemetry without metricReader -> Metric Reader defined outside of this SDK so we can use multiple Metrics Readers
-const sdk = new NodeSDK({
-  resource: resource,
-  traceExporter,
-  spanProcessors: [new BatchSpanProcessor(traceExporter)],
-  logRecordProcessor: new BatchLogRecordProcessor(logExporter),
-  instrumentations: [
-    getNodeAutoInstrumentations({
-      "@opentelemetry/instrumentation-aws-lambda": { enabled: false },
-      "@opentelemetry/instrumentation-fs": { enabled: false },
-      "@opentelemetry/instrumentation-winston": { enabled: false },
-    }),
-    new WinstonInstrumentation({
-      enabled: true,
-      disableLogSending: true,
-    }),
-  ],
-});
+  // Set this MeterProvider to be global to the app being instrumented.
+  metrics.setGlobalMeterProvider(meterProvider);
 
-// Start the SDK
-try {
-  sdk.start();
-  console.log("OpenTelemetry SDK started with auto-instrumentation");
-} catch (error) {
-  console.error("Error starting OpenTelemetry SDK:", error);
+  // Node SDK for OpenTelemetry
+  const sdk = new NodeSDK({
+    resource: resource,
+    traceExporter,
+    spanProcessors: [new BatchSpanProcessor(traceExporter)],
+    logRecordProcessor: new BatchLogRecordProcessor(logExporter),
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        "@opentelemetry/instrumentation-aws-lambda": { enabled: false },
+        "@opentelemetry/instrumentation-fs": { enabled: false },
+        "@opentelemetry/instrumentation-winston": { enabled: false },
+      }),
+      new WinstonInstrumentation({
+        enabled: true,
+        disableLogSending: true,
+      }),
+    ],
+  });
+
+  // Start the SDK
+  try {
+    await sdk.start();
+    console.log("OpenTelemetry SDK started with auto-instrumentation");
+  } catch (error) {
+    console.error("Error starting OpenTelemetry SDK:", error);
+  }
+
+  // Graceful shutdown
+  process.on("SIGTERM", () => {
+    sdk
+      .shutdown()
+      .then(() => console.log("SDK shut down successfully"))
+      .catch((error) => console.log("Error shutting down SDK", error))
+      .finally(() => process.exit(0));
+  });
+
+  return {
+    sdk,
+    meter: metrics.getMeter(
+      config.openTelemetry.SERVICE_NAME,
+      config.openTelemetry.SERVICE_VERSION,
+    ),
+  };
 }
 
-// Graceful shutdown
-process.on("SIGTERM", () => {
-  sdk
-    .shutdown()
-    .then(() => console.log("SDK shut down successfully"))
-    .catch((error) => console.log("Error shutting down SDK", error))
-    .finally(() => process.exit(0));
+export let otelSDK: NodeSDK;
+export let meter: ReturnType<typeof metrics.getMeter>;
+
+setupTelemetry().then(({ sdk, meter: setupMeter }) => {
+  otelSDK = sdk;
+  meter = setupMeter;
 });
 
-export const otelSDK = sdk;
-export const meter = metrics.getMeter(
-  config.openTelemetry.SERVICE_NAME,
-  config.openTelemetry.SERVICE_VERSION,
-);
+export function isOtelReady(): boolean {
+  return !!otelSDK && !!meter;
+}
